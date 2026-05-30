55cef323b77f730e738855c48c7b7dafa6c66da9
Avik Chowdhury <avikc323@outlook.com>
2026-05-30 22:59:14 +0530

    fix

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -2,19 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { LocalePreferenceService } from './locale-preference.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private localePreference: LocalePreferenceService,
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.auth.getToken();
+    const preference = this.localePreference.currentPreference;
+    const headers: Record<string, string> = {
+      'X-Display-Locale': preference.locale,
+      'X-Display-Currency': preference.currencyCode,
+      'X-Display-Exchange-Rate': preference.usdExchangeRate.toString(),
+    };
+
     if (token) {
-      const cloned = req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` }
-      });
-      return next.handle(cloned);
+      headers['Authorization'] = `Bearer ${token}`;
     }
-    return next.handle(req);
+
+    return next.handle(
+      req.clone({
+        setHeaders: headers,
+      }),
+    );
   }
 }
